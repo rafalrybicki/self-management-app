6ecb621ad04d9226d3e19b1d4a9e3b76af31c846
Rafał Rybicki <rafarybicki@gmail.com>
2023-05-11 18:44:40 +0200

    add Menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
 import { DayComponent } from './pages/day/day.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    DayComponent
+    DayComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
