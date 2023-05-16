b2d4d4ed7ad0d9603181dd9f335b872efdab9c42
Rafał Rybicki <rafarybicki@gmail.com>
2023-05-16 16:30:41 +0200

    add subtasks to Task component

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -9,17 +9,32 @@ import { showQuote } from '../../shared/utils';
 export class TaskComponent {
   @Input() id: number;
   @Input() content: string;
-  @Input() completed: boolean;
   @Input() date: string;
+  @Input() weight: number = 100;
+  @Input() completion: number = 0;
+  @Input() totalSubtasks: number = 0;
+  @Input() completedSubtasks: number = 0;
 
   toggle(): void {
     if (!this.completed) {
+      if (this.totalSubtasks === 0) {
+        this.completion = this.weight;
+      } else {
+        this.completedSubtasks += 1;
+        this.completion = (this.completedSubtasks / this.totalSubtasks) * this.weight;
+      }
       showQuote();
+    } else {
+      this.completedSubtasks = 0;
+      this.completion = 0;
     }
-    this.completed = !this.completed;
   }
 
   showDetails(): void {
     alert('details');
+  }
+
+  get completed(): boolean {
+    return this.completion === this.weight;
   }
 }
